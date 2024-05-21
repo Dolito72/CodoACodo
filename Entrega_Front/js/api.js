acdafa9cbdc20a08f8a57b51dd7ba14b21d62a6e
Maria Dolores Parral <doloresparral@hotmail.com>
2024-05-21 18:04:45 -0300

    modifico el tamaño de las cards q me traigo de la api

--- Entrega_Front/js/api.js
+++ Entrega_Front/js/api.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //clave de TMDB
+    // Clave de TMDB
     const apiKey = 'efe0bf78b5e791ce214d15556e76577b';  
     fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=es-ES`)
         .then(response => response.json())
@@ -8,8 +8,10 @@ document.addEventListener('DOMContentLoaded', function() {
             data.results.forEach(movie => {
                 const movieElement = document.createElement('div');
                 movieElement.className = 'movie-card';
+                // Usar una URL base para las imágenes y agregar el tamaño deseado
+                const imageUrl = `https://image.tmdb.org/t/p/w300${movie.poster_path}`;
                 movieElement.innerHTML = `
-                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                    <img src="${imageUrl}" alt="${movie.title}">
                     <h3>${movie.title}</h3>
                     <p>${movie.overview}</p>
                 `;
